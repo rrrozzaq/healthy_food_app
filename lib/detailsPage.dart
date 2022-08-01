5fe8953ae1b705ad1b09548894b47e6740afafbc
rrrozzaq <abdrozzaq02@gmail.com>
2022-08-01 14:20:00 +0700

    merapikan indentasi

--- lib/detailsPage.dart
+++ lib/detailsPage.dart
@@ -38,61 +38,61 @@ class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 64, 188, 29),
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(Icons.arrow_back_ios),
-            color: Colors.white,
-          ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          title: Text('Details',
-              style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: 18.0,
-                  color: Colors.white)),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.more_horiz),
-              onPressed: () {},
-              color: Colors.white,
-            )
-          ],
+      backgroundColor: Color.fromARGB(255, 64, 188, 29),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
         ),
-        body: ListView(children: [
-          Stack(children: [
-            Container(
-                height: MediaQuery.of(context).size.height - 82.0,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.transparent),
-            Positioned(
-                top: 75.0,
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45.0),
-                          topRight: Radius.circular(45.0),
-                        ),
-                        color: Colors.white),
-                    height: MediaQuery.of(context).size.height - 100.0,
-                    width: MediaQuery.of(context).size.width)),
-            Positioned(
-                top: 30.0,
-                left: (MediaQuery.of(context).size.width / 2) - 100.0,
-                child: Hero(
-                    tag: widget.heroTag,
-                    child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(widget.heroTag),
-                                fit: BoxFit.cover)),
-                        height: 200.0,
-                        width: 200.0))),
-            Positioned(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text('Details',
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontSize: 18.0, color: Colors.white)),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {},
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height - 82.0,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.transparent),
+              Positioned(
+                  top: 75.0,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(45.0),
+                            topRight: Radius.circular(45.0),
+                          ),
+                          color: Colors.white),
+                      height: MediaQuery.of(context).size.height - 100.0,
+                      width: MediaQuery.of(context).size.width)),
+              Positioned(
+                  top: 30.0,
+                  left: (MediaQuery.of(context).size.width / 2) - 100.0,
+                  child: Hero(
+                      tag: widget.heroTag,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(widget.heroTag),
+                                  fit: BoxFit.cover)),
+                          height: 200.0,
+                          width: 200.0))),
+              Positioned(
                 top: 250.0,
                 left: 25.0,
                 right: 25.0,
@@ -207,73 +207,80 @@ class _DetailsPageState extends State<DetailsPage> {
                       ),
                     )
                   ],
-                ))
-          ])
-        ]));
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildInfoCard(String cardTitle, String info, String unit) {
     return InkWell(
-        onTap: () {
-          selectCard(cardTitle);
-        },
-        child: AnimatedContainer(
-            duration: Duration(milliseconds: 500),
-            curve: Curves.easeIn,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
+      onTap: () {
+        selectCard(cardTitle);
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeIn,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: cardTitle == selectedCard
+              ? Color.fromARGB(255, 64, 188, 29)
+              : Colors.white,
+          border: Border.all(
               color: cardTitle == selectedCard
-                  ? Color.fromARGB(255, 64, 188, 29)
-                  : Colors.white,
-              border: Border.all(
-                  color: cardTitle == selectedCard
-                      ? Colors.transparent
-                      : Colors.grey.withOpacity(0.3),
-                  style: BorderStyle.solid,
-                  width: 0.75),
+                  ? Colors.transparent
+                  : Colors.grey.withOpacity(0.3),
+              style: BorderStyle.solid,
+              width: 0.75),
+        ),
+        height: 100.0,
+        width: 100.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, left: 15.0),
+              child: Text(cardTitle,
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 12.0,
+                    color: cardTitle == selectedCard
+                        ? Colors.white
+                        : Colors.grey.withOpacity(0.7),
+                  )),
             ),
-            height: 100.0,
-            width: 100.0,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, bottom: 8.0),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0, left: 15.0),
-                    child: Text(cardTitle,
-                        style: TextStyle(
+                children: <Widget>[
+                  Text(info,
+                      style: TextStyle(
                           fontFamily: 'Montserrat',
-                          fontSize: 12.0,
+                          fontSize: 14.0,
                           color: cardTitle == selectedCard
                               ? Colors.white
-                              : Colors.grey.withOpacity(0.7),
-                        )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, bottom: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(info,
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 14.0,
-                                color: cardTitle == selectedCard
-                                    ? Colors.white
-                                    : Colors.black,
-                                fontWeight: FontWeight.bold)),
-                        Text(unit,
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 12.0,
-                              color: cardTitle == selectedCard
-                                  ? Colors.white
-                                  : Colors.black,
-                            ))
-                      ],
-                    ),
-                  )
-                ])));
+                              : Colors.black,
+                          fontWeight: FontWeight.bold)),
+                  Text(unit,
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 12.0,
+                        color: cardTitle == selectedCard
+                            ? Colors.white
+                            : Colors.black,
+                      ))
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   selectCard(cardTitle) {
